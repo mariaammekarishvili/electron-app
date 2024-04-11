95d4a324bf1a0b74d70ac4e4df5b1c626a01d8d9
PersonalMariamMek <mariam.mekarishvili@gmail.com>
2024-04-11 11:53:41 +0400

    feat: on/off button

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
+var isVideo = true;
+
 navigator.mediaDevices
   .getUserMedia({ video: true })
-  .then(function (stream) {
+  .then(function(stream) {
     document.getElementById("camera").srcObject = stream;
   })
-  .catch(function () {
-    alert("could not connect stream");
+  .catch(function() {
+    alert("Could not connect stream");
   });
+
+function switchFunction() {
+  var videoElement = document.getElementById("camera");
+  
+  if (isVideo) {
+    videoElement.srcObject.getTracks().forEach(track => track.stop()); // Stop all video tracks
+    isVideo = false;
+    document.getElementById("switcher").innerHTML = "Camera On";
+  } else {
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then(function(stream) {
+        videoElement.srcObject = stream;
+      })
+      .catch(function() {
+        alert("Could not connect stream");
+      });
+    isVideo = true; // Update camera state
+    document.getElementById("switcher").innerHTML = "Camera Off";
+  }
+  console.log('I am here');
+}
--- main.js
+++ main.js
@@ -3,14 +3,14 @@ const path = require('node:path')
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 850,
+    height: 700,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
 
-  win.loadFile('camera.html')
+  win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
